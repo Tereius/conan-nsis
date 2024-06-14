3acd9d947bcaa85407a13d6533f1ccd3f53483ca
Björn Stresing <bjoern.stresing@gmx.de>
2024-06-14 23:53:26 +0200

    Update to conan 2

--- conanfile.py
+++ conanfile.py
@@ -1,29 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
-import os
+from conan import ConanFile
+from conan.tools.files import get, copy
+import json, os
 
+required_conan_version = ">=2.0"
 
 class NsisConan(ConanFile):
-    name = "NSIS"
-    version = "3.03"
-    description = "NSIS (Nullsoft Scriptable Install System) is a professional open source system to create Windows installers."
-    homepage = "https://sourceforge.net/projects/nsis"
-    url = "https://github.com/Tereius/conan-nsis"
-    license = "zlib-acknowledgement"
-    settings = {"os_build": ["Windows"], "arch_build": ["x86_64"]}
+
+    jsonInfo = json.load(open("info.json", 'r'))
+    # ---Package reference---
+    name = jsonInfo["projectName"]
+    version = jsonInfo["version"]
+    user = jsonInfo["domain"]
+    channel = "stable"
+    # ---Metadata---
+    description = jsonInfo["projectDescription"]
+    license = jsonInfo["license"]
+    author = jsonInfo["vendor"]
+    topics = jsonInfo["topics"]
+    homepage = jsonInfo["homepage"]
+    url = jsonInfo["repository"]
+    # ---Requirements---
+    requires = []
+    tool_requires = []
+    # ---Sources---
+    exports = ["info.json"]
+    exports_sources = []
+    # ---Binary model---
+    settings = "os", "arch"
+    options = {}
+    default_options = {}
+    # ---Build---
+    generators = []
+    # ---Folders---
+    no_copy_source = False
+
 
     def source(self):
-        source_url = "https://sourceforge.net/projects/nsis/files/NSIS%203/3.03/nsis-3.03.zip/download?use_mirror=autoselect"
-        tools.get(source_url, filename="nsis-%s.zip" % self.version)
+        source_url = "https://sourceforge.net/projects/nsis/files/NSIS%203/" + self.version + "/nsis-" + self.version + ".zip/download?use_mirror=autoselect"
+        get(self, source_url, filename="nsis-%s.zip" % self.version)
 
     def package(self):
-        self.copy(pattern="*", src="nsis-%s" % self.version)
-
-    def package_id(self):
-        self.info.include_build_settings()
+        copy(self, pattern="*", dst=self.package_folder, src="nsis-%s" % self.version)
 
     def package_info(self):
-        self.output.info('Adding NSIS binaries to PATH: %s' % self.package_folder)
-        self.env_info.PATH.append(self.package_folder)
+        self.output.info('Prepending to PATH environment variable: %s' % self.package_folder)
+        self.buildenv_info.prepend_path("PATH", self.package_folder)
